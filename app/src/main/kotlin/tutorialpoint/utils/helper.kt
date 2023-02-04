0e297b6741fcd635bb7e0ac5a2bd6ca4b3ae4e47
lim6112j <lim6112j@gmail.com>
2023-02-04 23:24:03 +0900

    formatting

--- app/src/main/kotlin/tutorialpoint/utils/helper.kt
+++ app/src/main/kotlin/tutorialpoint/utils/helper.kt
@@ -1,9 +1,7 @@
 package tutorialpoint.utils
-/**
- * Executes the given [block] and returns elapsed time in milliseconds.
- */
+/** Executes the given [block] and returns elapsed time in milliseconds. */
 public inline fun measureTimeMillis(block: () -> Unit): String {
     val start = System.currentTimeMillis()
     block()
-    return "Block took time " +(System.currentTimeMillis() - start).toString() + " milliseconds"
+    return "Block took time " + (System.currentTimeMillis() - start).toString() + " milliseconds"
 }
