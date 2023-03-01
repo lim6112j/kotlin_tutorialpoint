5eff73a09537cd6b1d2b597776c2a84f3dc3bc02
lim6112j <lim6112j@gmail.com>
2023-03-02 03:53:06 +0900

    version mismatch resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@
  * For more details take a look at the 'Building Java & JVM projects' chapter in the Gradle
  * User Manual available at https://docs.gradle.org/7.4.2/userguide/building_java_projects.html
  */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
@@ -43,4 +44,10 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClass.set("tutorialpoint.AppKt")
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
