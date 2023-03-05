e359628252d12398c5340a7b20e3a2474c4177df
lim6112j <lim6112j@gmail.com>
2023-03-05 15:16:14 +0900

    arrow test

--- app/src/main/kotlin/tutorialpoint/arrow/arrow.kt
+++ app/src/main/kotlin/tutorialpoint/arrow/arrow.kt
@@ -1,1 +1,9 @@
 package tutorialpoint.arrow
+import arrow.core.Either
+import arrow.core.computations.nullable.null
+object Lettuce
+object Knife
+object Salad
+fun takeFoodFromRefrigerator(): Lettuce? = null
+fun getKnife(): Knife? = null
+fun prepare(tool: Knife, ingredient: Lettuce): Salad = Salad
---
+++ app/src/test/kotlin/tutorialpoint/arrow/ArrowTest.kt
@@ -0,0 +1,7 @@
+package tutorialpoint.arrow
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertFails
+import kotlin.test.assertEquals
+import tutorialpoint.arrow.*
