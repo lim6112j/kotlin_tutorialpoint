f30bff6492ec92c6cf72c0962514621c1e6f1479
ben lim <lim6112j@gmail.com>
2023-03-07 16:02:48 +0900

    nullable  computation expression

--- app/src/main/kotlin/tutorialpoint/arrow/arrow.kt
+++ app/src/main/kotlin/tutorialpoint/arrow/arrow.kt
@@ -8,9 +8,17 @@ object Salad
 fun takeFoodFromRefrigerator(): Lettuce? = null
 fun getKnife(): Knife? = null
 fun prepare(tool: Knife, ingredient: Lettuce): Salad = Salad
-fun prepareLunch(): Salad? {
-  val lettuce = takeFoodFromRefrigerator()
-  val knife = getKnife()
-  val salad = knife?.let {k -> lettuce?.let {l -> prepare(k, l)}}
-  return salad
+//fun prepareLunch(): Salad? {
+//  val lettuce = takeFoodFromRefrigerator()
+//  val knife = getKnife()
+//  val salad = knife?.let {k -> lettuce?.let {l -> prepare(k, l)}}
+//  return salad
+//}
+suspend fun prepareLunch(): Salad? {
+  nullable {
+    val lettuce = takeFoodFromRefrigerator().bind()
+    val knife = getKnife().bind()
+    val salad = prepare(knife, lettuce).bind()
+    salad
+  }  
 }
