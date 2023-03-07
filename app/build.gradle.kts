637e51263129847fec5dcfce2198db4e6be8b595
ben lim <lim6112j@gmail.com>
2023-03-07 11:58:42 +0900

    again setting problem resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.6.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -44,9 +44,9 @@ application {
     // Define the main class for the application.
     mainClass.set("tutorialpoint.AppKt")
 }
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-}
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions {
+//        freeCompilerArgs = listOf("-Xjsr305=strict")
+//        jvmTarget = JavaVersion.VERSION_17.toString()
+//    }
+//}
