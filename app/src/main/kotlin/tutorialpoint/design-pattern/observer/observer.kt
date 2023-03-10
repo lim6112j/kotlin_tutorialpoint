64f90fdab5fba876a292ba9024ae2ec4692d979f
ben lim <lim6112j@gmail.com>
2023-03-10 15:44:54 +0900

    oop observer

--- app/src/main/kotlin/tutorialpoint/design-pattern/observer/observer.kt
+++ app/src/main/kotlin/tutorialpoint/design-pattern/observer/observer.kt
@@ -1,16 +1,25 @@
-interface Subscriber {
+package tutorialpoint.designpattern.observer
+interface OOPSubscriber {
+  val name: String
+  val id: Int
   fun update(): Unit
-  fun subscribe(): Unit
+  fun subscribe(publisher: OOPPublisher): Unit
 }
-interface Publisher {
-  var subscribers: List<Subscriber>
-  fun notify(): Unit
+interface OOPPublisher {
+  val name: String
+  val id: Int
+  var subscribers: List<OOPSubscriber>
+  fun notifySub(): Unit
 }
-class NewsSubscriber : Subscriber {
+class NewsSubscriber( val n: String,  val i: Int) : OOPSubscriber {
+  override val name = n
+  override val id = i
   override fun update(): Unit = TODO()
-  override fun subscribe(): Unit = TODO()
+  override fun subscribe(publisher:OOPPublisher): Unit = TODO()
 }
-class NewsPublisher(var subs: List<Subscriber>) : Publisher {
+class NewsPublisher(val n:String, val i:Int, var subs: List<OOPSubscriber>) : OOPPublisher {
+  override val name = n
+  override val id = i
   override var subscribers = subs
-  override fun notify(): Unit = TODO()
+  override fun notifySub(): Unit = TODO()
 }
