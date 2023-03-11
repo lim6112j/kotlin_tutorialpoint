25501f5f809fe4b1aa9dcef7f5803536ea7e83b2
ben lim <lim6112j@gmail.com>
2023-03-11 12:11:11 +0900

    polished

--- app/src/main/kotlin/tutorialpoint/design-pattern/command/command.kt
+++ app/src/main/kotlin/tutorialpoint/design-pattern/command/command.kt
@@ -3,11 +3,14 @@ sealed class UIComponent
 object Button:UIComponent()
 object Shortcut: UIComponent()
 object Menu: UIComponent()
+
 sealed class Command
 object CopyCommand: Command()
 object PasteCommand: Command()
 object CutCommand: Command()
+
 data class Invoker(val queue: List<Pair<UIComponent, Command>>)
+
 fun Invoker.execute(component: UIComponent, command: Command): Invoker = 
   this.addCommandToQueue(component, command)
 fun Invoker.addCommandToQueue(component: UIComponent, command: Command): Invoker {
